b1a118b5822f9f94d6ba82d0db9d11f3e1adbec8
Cristine <cris.scheib@hotmail.com>
2025-06-25 16:42:21 -0300

    starting the project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# fiap-machine-learning-tech-challenge-11
-Tech Challenge 1 - Pós Tech Engenharia de Machine Learning
+# FIAP Machine Learning Engineering - Tech Challenge 1
+
+//TODO breve descrição
+
+![image]()
+
+-----------------------------------
+
+1. [Instalação](#installation)
+2. [Estrutura do Projeto](#structure)
+3. [Licenciamento, Autores e Agradecimentos](#licensing)
+
+## Instalação <a name="installation"></a>
+
+
+## Estrutura do Projeto <a name="structure"></a>
+
+![image]()
+
+## Licenciamento, Autores e Agradecimentos<a name="licensing"></a>
---
+++ api/app/controllers/user_controller.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.schemas.user_schema import UserCreate, UserOut, Token
+from app.core.database import get_db
+from app.services import user_service
+from app.core.auth import (
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+)
+
+router = APIRouter()
+
+@router.post("/", response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = user_service.get_user_by_username(db, user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+    user_obj = user_service.create_user(db, user)
+    return user_obj
+
+
+@router.post("/token", response_model=Token)
+def login_for_access_token(
+    db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=401, detail="User or password incorrect")
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/me", response_model=UserOut)
+def read_users_me(current_user=Depends(get_current_user)):
+    return current_user
