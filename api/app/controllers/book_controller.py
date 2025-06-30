d33bddfebf56cf644466745b9dddf63fadee04d0
GuilhermeDellatin <guidellatin@gmail.com>
2025-06-29 22:05:23 -0300

    Adding book controller and include your prefix in routes.py

---
+++ api/app/controllers/book_controller.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+from api.app.core.database import get_db
+from api.app.models.book_model import Book
+from api.app.schemas.book_schema import BookSchema
+
+router = APIRouter()
+
+@router.get("/books", response_model=List[BookSchema])
+def get_books(db: Session = Depends(get_db)):
+    books = db.query(Book).all()
+    return books
