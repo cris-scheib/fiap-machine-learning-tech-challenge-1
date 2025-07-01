1e659d6beaba6b48b1c95a2903b5328b099c75c9
GuilhermeDellatin <guidellatin@gmail.com>
2025-06-30 22:01:04 -0300

    Adjusts

--- api/app/controllers/book_controller.py
+++ api/app/controllers/book_controller.py
@@ -5,30 +5,10 @@ from typing import List
 from app.core.database import get_db
 from app.models.book_model import Book
 from app.schemas.book_schema import BookSchema
+from app.services import books_service
 
 router = APIRouter()
 
-@router.get("/books", response_model=List[BookSchema])
-def get_books(db: Session = Depends(get_db)):
-    try:
-        books = db.query(Book).all()
-
-        if not books:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Nenhum livro encontrado na base de dados."
-            )
-
-        return books
-
-    except SQLAlchemyError as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro ao acessar o banco de dados: {str(e)}"
-        )
-
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro inesperado: {str(e)}"
-        )
+@router.get("/", response_model=List[BookSchema])
+def list_books(db: Session = Depends(get_db)):
+    return get_all_books(db)
