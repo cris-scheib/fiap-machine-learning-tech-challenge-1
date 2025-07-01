c014f4c8bd5afbc9918949b781f06f088d450223
GuilhermeDellatin <guidellatin@gmail.com>
2025-06-30 21:31:25 -0300

    Adjust routes.py

--- api/app/routes.py
+++ api/app/routes.py
@@ -3,5 +3,5 @@ from app.controllers import health_controller, book_controller, user_controller
 
 router = APIRouter()
 router.include_router(user_controller.router, prefix="/users", tags=["Users"])
-router.include_router(book_controller.router, prefix="/api/v1/books", tags=["Books"])
+router.include_router(book_controller.router, prefix="/api/v1", tags=["Books"])
 router.include_router(health_controller.router, tags=["Health"])  
