c4238bf0b604c87723f06983b67749e99137f967
GuilhermeDellatin <guidellatin@gmail.com>
2025-07-08 22:20:24 -0300

    Adding auth in books endpoints

--- api/app/controllers/book_controller.py
+++ api/app/controllers/book_controller.py
@@ -5,11 +5,16 @@ from app.core.database import get_db
 from app.models.book_model import Book
 from app.schemas.book_schema import BookSchema
 from app.services import books_service
+from app.core.auth import get_current_user
 
-router = APIRouter()
+router = APIRouter(
+    dependencies=[Depends(get_current_user)]
+)
 
 @router.get("/", response_model=List[BookSchema])
-def list_books(db: Session = Depends(get_db)): return books_service.get_all_books(db)
+def list_books(
+        db: Session = Depends(get_db),
+): return books_service.get_all_books(db)
 
 @router.get("/search", response_model=List[BookSchema])
 def list_books_by_title_and_category(title: str, category: Optional[str] = None, db: Session = Depends(get_db)):
