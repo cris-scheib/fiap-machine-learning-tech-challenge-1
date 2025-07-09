d09f1c2f0cf49cfc56370fdd0422238737fb1400
GuilhermeDellatin <guidellatin@gmail.com>
2025-07-08 22:19:59 -0300

    Adding auth in books endpoints

--- api/app/controllers/category_controller.py
+++ api/app/controllers/category_controller.py
@@ -4,8 +4,11 @@ from typing import List
 from app.core.database import get_db
 from app.services.category_service import get_all_categories
 from app.schemas.category_schema import CategorySchema
+from app.core.auth import get_current_user
 
-router = APIRouter()
+router = APIRouter(
+    dependencies=[Depends(get_current_user)]
+)
 
 @router.get("/", response_model=List[CategorySchema])
 def list_categories(db: Session = Depends(get_db)):
