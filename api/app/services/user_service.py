e982972ee292011ea77afd891f2e4a8bd0535049
GuilhermeDellatin <guidellatin@gmail.com>
2025-08-03 18:10:09 -0300

    Adjust comparison in user_service.py

--- api/app/services/user_service.py
+++ api/app/services/user_service.py
@@ -4,7 +4,7 @@ from app.schemas.user_schema import UserCreate
 from app.core.auth import get_password_hash
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(User).filter(User.username == username).first()
+    return db.query(User).filter(username == User.username).first()
 
 def create_user(db: Session, user: UserCreate):
     hashed_password = get_password_hash(user.password)
